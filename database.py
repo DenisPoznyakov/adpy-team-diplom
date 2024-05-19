e613fb4e7a2d18369451d506cdab6d2665f7b146
Alexandr Kalinkin <akalinkin84@yandex.ru>
2024-05-19 14:33:55 +0300

    6

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ port = input('Введите порт сервера: ')
 name_db = input('Введите название БД: ')
 
 
-DSN = f'{driver_db}://{login}:{password}@{host}:{port}/{name_db}
+DSN = f'{driver_db}://{login}:{password}@{host}:{port}/{name_db}'
 
 engine = sqlalchemy.create_engine(DSN)
 
