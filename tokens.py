2f2386b2101af54483346641de76d3e6c92a93ec
Alexandr Kalinkin <akalinkin84@yandex.ru>
2024-06-06 11:26:18 +0300

    token

--- tokens.py
+++ tokens.py
@@ -1,3 +1,3 @@
-token = 'vk1.a.Srf15C6wGCBcMObPuY6ZoKfAzAp57qt10wUIwtMyBAL-yXDYu41yoF7iQh1P6-LJVk4FiZS9YovCVpV4P0OirqDdxfcW8GP69dQHlVIkki127ir0be-jnQGzfgtHE5bhawiNWXZxChRsxpJqrbLtlL973c_YP8uPy0mq7KvFE2WLd0okWzKkLSyY7t5MAAuMRblfvFNA1k1snwWq8VjMBw'
-token_app = 'ddca84d2ddca84d2ddca84d2c0ded2c772dddcaddca84d2bbf5d222c3a66e248f563db7'
+token = input('Введите токен: ')
+token_app = input('Введите токен приложения: ')
 access_token = input('Введите ключ доступа пользователя: ')
